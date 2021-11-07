1d8828063975203e00e59d898d9aeafcc7ccf8d6
kodaiokawa <okawa@cns.s.u-tokyo.ac.jp>
2021-11-07 16:17:09 +0900

    [test]

---
+++ build/lib/CMakeFiles/refer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/refer.dir/source/beam.cpp.o"
+  "CMakeFiles/refer.dir/source/beam.cpp.o.d"
+  "CMakeFiles/refer.dir/source/function.cpp.o"
+  "CMakeFiles/refer.dir/source/function.cpp.o.d"
+  "CMakeFiles/refer.dir/source/mass.cpp.o"
+  "CMakeFiles/refer.dir/source/mass.cpp.o.d"
+  "librefer.a"
+  "librefer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/refer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/lib/CMakeFiles/refer.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "librefer.a"
+)
